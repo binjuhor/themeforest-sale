e6faf085fb42c67960f0e416f0c789b121088489
Kiem Hoang <kiemhd@outlook.com>
2019-09-02 16:08:21 +0700

    Add HTTPS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Binjuhor\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [
@@ -60,7 +61,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        '2fa' => \PragmaRX\Google2FALaravel\Middleware::class,
     ];
 
     /**
