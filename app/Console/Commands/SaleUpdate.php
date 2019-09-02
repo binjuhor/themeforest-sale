0929810a4d5ad1fb7081b88d963f8a845b30848e
Kiem Hoang <kiemhd@outlook.com>
2019-09-02 07:18:13 +0700

    Create command update

---
+++ app/Console/Commands/SaleUpdate.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use PHPHtmlParser\Dom;
+use App\Repositories\Eloquent\EloquentSalesRepositoryRepository as SaleRepository;
+
+class SaleUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sale:update {tfuser}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update sale for a user on themeforest';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+
+    protected $sales;
+
+    public function __construct(SaleRepository $sale)
+    {
+        parent::__construct();
+        $this->sales = $sale;
+    }
+
+    /**
+     * @throws \PHPHtmlParser\Exceptions\ChildNotFoundException
+     * @throws \PHPHtmlParser\Exceptions\CircularException
+     * @throws \PHPHtmlParser\Exceptions\CurlException
+     * @throws \PHPHtmlParser\Exceptions\NotLoadedException
+     * @throws \PHPHtmlParser\Exceptions\StrictException
+     */
+    public function handle()
+    {
+        return $this->saleUpdate();
+    }
+
+    /**
+     * @throws \PHPHtmlParser\Exceptions\ChildNotFoundException
+     * @throws \PHPHtmlParser\Exceptions\CircularException
+     * @throws \PHPHtmlParser\Exceptions\CurlException
+     * @throws \PHPHtmlParser\Exceptions\NotLoadedException
+     * @throws \PHPHtmlParser\Exceptions\StrictException
+     */
+    public function saleUpdate()
+    {
+        $themeuser = $this->argument('tfuser');
+        $dom = new Dom;
+        $dom->loadFromUrl("https://themeforest.net/user/{$themeuser}");
+        $textSale = $dom->find('.user-info-header__stats-content strong', 0);
+        $salesThisMonth = str_replace(',', '', trim($textSale->text));
+
+        $this->sales->createOrUpdate([
+            'author' => $themeuser,
+            'name' => 'T' . date('m/Y'),
+            'sale' => "{$salesThisMonth}",
+        ]);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('sale:update beautheme --force')->hourly();
     }
 
     /**
