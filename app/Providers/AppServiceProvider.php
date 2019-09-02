b22b52c8ad7f0dd6a80a0f27eae95ecff3015611
Kiem Hoang <kiemhd@outlook.com>
2019-09-02 16:20:06 +0700

    Remove the HTTPS middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        //Force URL to HTTPS protocol
+        if (env('APP_ENV') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
